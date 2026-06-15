a93948b0f347d9b30b6b19cfa59687ff810b6d75
Rishi <rishi@Rishi-MacBook.local>
2026-06-15 15:44:14 -0700

    first grid update

--- main.py
+++ main.py
@@ -20,15 +20,14 @@ class Button():
 
 pygame.init()
 
-red =  Button(692, 148, 56, 56, (255, 0, 0))
-black = Button(692, 212, 56, 56, (0, 0, 0))
-white = Button(692, 276, 56, 56, (255, 255, 255))
-green = Button(692, 340, 56, 56, (0, 255, 0))
-blue = Button(692, 404, 56, 56, (0, 0, 255))
-yellow = Button(692, 468, 56, 56, (255, 255, 0))
-magenta = Button(692, 532, 56, 56, (255, 0, 255))
-cyan = Button(692, 596, 56, 56, (0, 255, 255))
-
+red =  Button(692, 148, 56, 56, (255, 0, 0)) # 1
+black = Button(692, 212, 56, 56, (0, 0, 0)) # 2
+white = Button(692, 276, 56, 56, (255, 255, 255)) # 3
+green = Button(692, 340, 56, 56, (0, 255, 0)) # 4
+blue = Button(692, 404, 56, 56, (0, 0, 255)) # 5
+yellow = Button(692, 468, 56, 56, (255, 255, 0)) # 6
+magenta = Button(692, 532, 56, 56, (255, 0, 255)) # 7
+cyan = Button(692, 596, 56, 56, (0, 255, 255)) # 8
 
 
 
@@ -40,6 +39,12 @@ clock = pygame.time.Clock()
 
 side_length_of_grids = None
 start = True
+
+pencil = True
+eraser = False
+current_color = white.color
+
+user_out_of_bounds = False
 
 text_entry = pygame_gui.elements.UITextEntryLine(
     relative_rect=pygame.Rect(100, 200, 200, 40),
@@ -53,16 +58,19 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = event.pos
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN and start:
             user_entry_text = text_entry.get_text()
             side_length_of_grids = int(user_entry_text)
+            grid_height = side_length_of_grids
+            grid_width = side_length_of_grids
             start = False
             text_entry.kill()
 
         manager.process_events(event)
     
     pygame.display.update()
-   
 
     window.fill((255, 255, 255))
 
@@ -75,44 +83,102 @@ while running:
         window.blit(starting_text, (50, 50))
     elif not start:
         red.draw(window)
-        cyan.draw(window)
-        yellow.draw(window)
         black.draw(window)
-        blue.draw(window)
-        magenta.draw(window)
         white.draw(window)
         green.draw(window)
-        grid_drawn = False
-        if not grid_drawn:
-            pixels_side_length = 640 // side_length_of_grids
-            stopping_point_for_iteration = pixels_side_length * side_length_of_grids
-            # pixels_side_length is how many pixels per grid length AND width,
-            #  and side_length_of_grids is how many GRIDS length AND width
-            iteration = 0
-            for x in range(0, stopping_point_for_iteration, pixels_side_length):
-                pygame.draw.line(
-                    window, 
-                    color=(0,0,0), 
-                    start_pos=(iteration * pixels_side_length, 640), 
-                    end_pos=(iteration * pixels_side_length, 0), 
-                    width=1
-                )
-                iteration += 1
+        blue.draw(window)
+        yellow.draw(window)
+        magenta.draw(window)
+        cyan.draw(window) 
+        #                           for later::
+        # the way you want to do it is you find where the mouse is using events
+        # if the mouse position mod pixels_side_length(x & y separatly) is 0, 
+        # tell user to pick definite box
+        # if the mouse position mode pixels_side_length is not 0, 
+        # figure out what box they are in using integer divisoin PLUS 1
+        # color that square
+        
+        pixels_side_length = 640 // side_length_of_grids
+        stopping_point_for_iteration = pixels_side_length * side_length_of_grids
+        # pixels_side_length is how many pixels per grid length AND width,
+        #  and side_length_of_grids is how many GRIDS length AND width
+        iteration = 0
+        for x in range(0, stopping_point_for_iteration, pixels_side_length):
+            pygame.draw.line(
+                window, 
+                color=(0,0,0), 
+                start_pos=(iteration * pixels_side_length, 640), 
+                end_pos=(iteration * pixels_side_length, 0), 
+                width=1
+            )
+            iteration += 1
 
-            iteration = 0
-            for y in range(0, stopping_point_for_iteration, pixels_side_length):
-                pygame.draw.line(
-                    window, 
-                    color=(0,0,0),
-                    start_pos=(0, iteration * pixels_side_length), 
-                    end_pos=(640 ,iteration * pixels_side_length),
-                    width=1
-                )
-                iteration += 1
+        iteration = 0
+        for y in range(0, stopping_point_for_iteration, pixels_side_length):
+            pygame.draw.line(
+                window, 
+                color=(0,0,0),
+                start_pos=(0, iteration * pixels_side_length), 
+                end_pos=(640 ,iteration * pixels_side_length),
+                width=1
+            )
+            iteration += 1
+        
+        grid = []
+        for row in range(side_length_of_grids):
+            grid_row = []
+            for col in range(side_length_of_grids):
+                grid_row.append(white.color)
+            grid.append(grid_row)
+        grid_drawn = True
+    # mouse clicking und button management
+        print("here")
+        #button clicking
+        # color menu
+        if red.rect.collidepoint(mouse_x,mouse_y):
+            red.selected
+            current_color = red.color
+        elif black.rect.collidepoint(mouse_x,mouse_y):
+            black.selected
+            current_color = black.color
+        elif white.rect.collidepoint(mouse_x,mouse_y):
+            white.selected
+            current_color = white.color
+        elif green.rect.collidepoint(mouse_x,mouse_y):
+            green.selected
+            current_color = green.color
+        elif blue.rect.collidepoint(mouse_x,mouse_y):
+            blue.selected
+            current_color = blue.color
+        elif yellow.rect.collidepoint(mouse_x,mouse_y):
+            yellow.selected
+            current_color = yellow.color
+        elif magenta.rect.collidepoint(mouse_x,mouse_y):
+            magenta.selected
+            current_color = magenta.color
+        elif cyan.rect.collidepoint(mouse_x,mouse_y):
+            cyan.selected
+            current_color = cyan.color
+        #grid
+        if mouse_x > 0 and mouse_x > 640 and mouse_y > 0 and mouse_y < 640:
+            color_list = [red.color, black.color, white.color, green.color, blue.color, yellow.color, magenta.color, cyan.color]
+            for number in range(len(color_list)):
+                if current_color == color_list[number]:
+                    if mouse_x % pixels_side_length != 0 and mouse_y % pixels_side_length != 0:
+                        grid_mouse_x = mouse_x // 32 + 1
+                        grid_mouse_y = mouse_y // 32 + 1
+                        grid[grid_mouse_y][grid_mouse_x] = current_color
+                    else:
+                        user_out_of_bounds = True
+            print(grid)
+        #pencil
 
-            grid_drawn = True
-        else:
-            print("delete this later")
+        #eraser
+
+
+
+
+            
             
         
     
@@ -122,4 +188,4 @@ while running:
     manager.draw_ui(window)
     pygame.display.flip()
 
-pygame.quit()
+pygame.quit("here")
